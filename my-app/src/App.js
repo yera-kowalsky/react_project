4bc4b5b280535eea9ef8cd38d8fba15aa54bb6a8
Yerkebulan <yerkebulan014@gmail.com>
2023-02-25 14:49:09 +0100

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is my new react application</h1>
+      <h1>This is my new react app</h1>
     </div>
   );
 }
