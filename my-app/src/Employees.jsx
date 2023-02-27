d9f4cfd60f410b6c8e0acd982f26ed7073e87ed5
Yerkebulan <yerkebulan014@gmail.com>
2023-02-27 13:44:59 +0100

    updated

--- my-app/src/Employees.jsx
+++ my-app/src/Employees.jsx
@@ -1,7 +1,10 @@
 // usestate hook is used as a functinal component.Hook is special function that lets you hook into react features
 import {useState} from "react";
+import femaleProfile from './images/femaleProfile.jpg';
+import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
+    const [selectedTeam, setTeam] = useState("TeamB");
     // first item is the employee data, the state of the employees data is maintained in this item. The second item
     // is a function reference and is usd wherever we need to change the state of the employee's array stored in the first item here.
     // Basically the use state hook returns a pair of values.[current state, fucntion that updates it]
@@ -90,13 +93,48 @@ const Employees = () => {
             gender: "male",
             teamName: "TeamD"
         }])
+    function handleTeamSelectChange(event){
+
+        setTeam(event.target.value)
+    }
+
+    function handleEmployeeCardClick(event){
+        const transformedEmployees = empolyees.map((empolyee) => empolyee.id === parseInt(event.currentTarget.id)
+                                                  ?(empolyee.teamName === selectedTeam)?{...empolyee, teamName:''}:{...empolyee,
+                                                  teamName: selectedTeam}:empolyee);
+        setEmployees(transformedEmployees)
+    }
     return (
-        <main>
-            {
-                empolyees.map((empolyee)=>(
-                    <p>{empolyee.fullName}</p>
-                ))
-            }
+        <main className='container'>
+            <div className='row justify-content-center mt-3 mb-3'>
+                <div className='col-6'>
+                    <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectChange}>
+                        <option value='TeamA'>TeamA</option>
+                        <option value='TeamB'>TeamB</option>
+                        <option value='TeamC'>TeamC</option>
+                        <option value='TeamD'>TeamD</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class='row justify-content-center mt-3 mb-3'>
+                <div class='col-8'>
+                    <div class='card-collection'>
+                    {
+                        empolyees.map((employee) =>(
+                            <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{cursor: "pointer"}} onClick={handleEmployeeCardClick}>
+                                {(employee.gender === 'male')?<img src={maleProfile} className='card-img-top'/>
+                                                             :<img src={femaleProfile} className='card-img-top'/>}
+                                <div className='card-body'>
+                                    <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                                    <p className='card-text'><b>Designation:</b> {employee.designation}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
